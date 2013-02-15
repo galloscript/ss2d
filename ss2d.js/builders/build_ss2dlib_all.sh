51c66d8598b7d335987bfd09f27a8448df7af327
David Gallardo <portalg@gmail.com>
2013-02-15 21:50:46 +0100

    lib builds ignored

--- ss2d.js/builders/build_ss2dlib_all.sh
+++ ss2d.js/builders/build_ss2dlib_all.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-./build_ss2dlib.sh
+./build_ss2dlib_offline.sh
 ./build_ss2dlib_client.sh
 ./build_ss2dlib_server.sh
 
-./build_ss2dlib.sh -min
+./build_ss2dlib_offline.sh -min
 ./build_ss2dlib_client.sh -min
 ./build_ss2dlib_server.sh -min
